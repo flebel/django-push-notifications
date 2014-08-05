baa5c83dfeafcea95ca819ba44ef533408b93038
Francois Lebel <francoislebel@gmail.com>
2014-08-05 12:31:52 -0700

    Reworked the APNS engine to support certificates with a passphrase.

--- push_notifications/apns.py
+++ push_notifications/apns.py
@@ -4,6 +4,7 @@ Documentation is available on the iOS Developer Library:
 https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html
 """
 
+import OpenSSL
 import json
 import ssl
 import struct
@@ -30,11 +31,15 @@ class APNSServerError(APNSError):
 class APNSDataOverflow(APNSError):
 	pass
 
+class InvalidPassPhrase(APNSError):
+	def __init__(self, message='The passphrase for the private key appears to be invalid'):
+		super(InvalidPassPhrase, self).__init__(message)
+
 
 APNS_MAX_NOTIFICATION_SIZE = 256
 
 
-def _apns_create_socket():
+def _apns_create_connection():
 	certfile = SETTINGS.get("APNS_CERTIFICATE")
 	if not certfile:
 		raise ImproperlyConfigured(
@@ -43,15 +48,31 @@ def _apns_create_socket():
 
 	try:
 		with open(certfile, "r") as f:
-			f.read()
+			cert_content = f.read()
 	except Exception as e:
 		raise ImproperlyConfigured("The APNS certificate file at %r is not readable: %s" % (certfile, e))
 
-	sock = socket.socket()
-	sock = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_SSLv3, certfile=certfile)
-	sock.connect((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
-
-	return sock
+	# ssl in Python < 3.2 does not support certificates/keys as strings.
+	# See http://bugs.python.org/issue3823
+	# Therefore pyOpenSSL which lets us do this is a dependancy.
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_content)
+	with open(SETTINGS['APNS_PRIVATE_KEY'], "r") as f:
+		args = [OpenSSL.crypto.FILETYPE_PEM, f.read()]
+	if SETTINGS["APNS_PASSPHRASE"] is not None:
+		args.append(str(SETTINGS["APNS_PASSPHRASE"]))
+	try:
+		pkey = OpenSSL.crypto.load_privatekey(*args)
+	except OpenSSL.crypto.Error:
+		raise InvalidPassPhrase
+	context = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv3_METHOD)
+	context.use_certificate(cert)
+	context.use_privatekey(pkey)
+	connection = OpenSSL.SSL.Connection(context, sock)
+	connection.connect((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
+	connection.set_connect_state()
+	connection.do_handshake()
+	return connection
 
 
 def _apns_pack_frame(token_hex, payload, identifier, expiration, priority):
@@ -95,7 +116,7 @@ def _apns_check_errors(sock):
 
 
 def _apns_send(token, alert, badge=None, sound=None, content_available=False, action_loc_key=None, loc_key=None,
-				loc_args=[], extra={}, identifier=0, expiration=None, priority=10, socket=None):
+				loc_args=[], extra={}, identifier=0, expiration=None, priority=10, connection=None):
 	data = {}
 	aps_data = {}
 
@@ -134,12 +155,12 @@ def _apns_send(token, alert, badge=None, sound=None, content_available=False, ac
 
 	frame = _apns_pack_frame(token, json_data, identifier, expiration_time, priority)
 
-	if socket:
-		socket.write(frame)
+	if connection:
+		connection.send(frame)
 	else:
-		with closing(_apns_create_socket()) as socket:
-			socket.write(frame)
-			_apns_check_errors(socket)
+		with closing(_apns_create_connection()) as connection:
+			connection.send(frame)
+			_apns_check_errors(connection)
 
 
 def apns_send_message(registration_id, alert, **kwargs):
@@ -166,7 +187,8 @@ def apns_send_bulk_message(registration_ids, alert, **kwargs):
 	it won't be included in the notification. You will need to pass None
 	to this for silent notifications.
 	"""
-	with closing(_apns_create_socket()) as socket:
+	with closing(_apns_create_connection()) as connection:
 		for identifier, registration_id in enumerate(registration_ids):
-			_apns_send(registration_id, alert, identifier=identifier, socket=socket, **kwargs)
-		_apns_check_errors(socket)
+			_apns_send(registration_id, alert, identifier=identifier, connection=connection, **kwargs)
+		_apns_check_errors(connection)
+
